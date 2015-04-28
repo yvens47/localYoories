a9c4077c1d253a28f7c5b933e08d1a5d79bab4a2
jeanypierre <yvens47@yahoo.com>
2015-04-29 01:10:31 +0200

    fixing registration button

--- register.php
+++ register.php
@@ -33,7 +33,7 @@ require_once 'template/header.php';
                 </div>
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-default">Sign in</button>
+                        <button type="submit" class="btn btn-default">Sign Up</button>
                     </div>
                 </div>
             </form>
